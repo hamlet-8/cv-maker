6b12fa7865300d865683db81f581ed57a6d8a89a
Hamlet Poghosyan <hamlet.poghosyan.888@gmail.com>
2020-10-21 23:47:43 +0400

    version 1.5.6

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const jsonServer = request('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('./public.db.json')
-const middlewares = jsonServer.default({
-    static: './build',
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
 });
-
-const PORT = process.env.PORT || 3001;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(POST, () => {
-    console.log('Server is running');
-})
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);

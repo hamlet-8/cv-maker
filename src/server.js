d7657faebcb2997dca30d6436b25f49be68a380c
Hamlet Poghosyan <hamlet.poghosyan.888@gmail.com>
2020-10-21 23:36:38 +0400

    version 1.5.5

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
+const jsonServer = request('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./public.db.json')
 const middlewares = jsonServer.default({
     static: './build',
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
+server.use(router);
 
 server.listen(POST, () => {
     console.log('Server is running');
